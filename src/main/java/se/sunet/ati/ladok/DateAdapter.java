cb75fa8613cda1ba826481b9d512a48a9c983a36
Dennis Lundberg <dennisl@apache.org>
2017-06-22 07:49:25 +0200

    Lägg tillbaka Marcus ändringar för SELNY-399.

--- src/main/java/se/sunet/ati/ladok/DateAdapter.java
+++ src/main/java/se/sunet/ati/ladok/DateAdapter.java
@@ -1,8 +1,7 @@
 package se.sunet.ati.ladok;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import javax.xml.bind.DatatypeConverter;
 
 /**
@@ -15,20 +14,24 @@ import javax.xml.bind.DatatypeConverter;
  * @author Dennis Lundberg
  */
 public class DateAdapter {
+  // The use of SimpleDateFormat is because L3 expects the date to be passed without any timezone reference (black box tested)
+  // Previously this was solved by suppressing the TZ from XMLGregorianCalendar but now needs to be handled by this adapter
+  private static final SimpleDateFormat formatterDate = new SimpleDateFormat("yyyy-MM-dd");
+  private static final SimpleDateFormat formatterDateTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+
   public static Date parseDate(final String date) {
     return DatatypeConverter.parseDate(date).getTime();
   }
+
   public static String printDate(final Date date) {
-    Calendar cal = new GregorianCalendar();
-    cal.setTime(date);
-    return DatatypeConverter.printDate(cal);
+    return formatterDate.format(date);
   }
+
   public static Date parseDateTime(final String dateTime) {
     return DatatypeConverter.parseDateTime(dateTime).getTime();
   }
+
   public static String printDateTime(final Date dateTime) {
-    Calendar cal = new GregorianCalendar();
-    cal.setTime(dateTime);
-    return DatatypeConverter.printDateTime(cal);
+    return formatterDateTime.format(dateTime);
   }
 }
